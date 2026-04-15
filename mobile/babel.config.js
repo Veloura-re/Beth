dc747d0c75c2f989161f93dd117c28738de2967d
Liora <liora@example.com>
2026-04-15 05:56:38 +0300

    fm: stabilized mobile bundler with synchronized babel transforms

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -5,7 +5,14 @@ module.exports = function(api) {
     plugins: [
       '@babel/plugin-proposal-optional-chaining',
       '@babel/plugin-proposal-nullish-coalescing-operator',
-      '@babel/plugin-proposal-logical-assignment-operators'
+      '@babel/plugin-proposal-logical-assignment-operators',
+      '@babel/plugin-transform-template-literals',
+      '@babel/plugin-transform-arrow-functions',
+      '@babel/plugin-transform-shorthand-properties',
+      '@babel/plugin-transform-block-scoping',
+      ['@babel/plugin-transform-class-properties', { "loose": true }],
+      ['@babel/plugin-transform-private-methods', { "loose": true }],
+      ['@babel/plugin-transform-private-property-in-object', { "loose": true }]
     ],
   };
 };
