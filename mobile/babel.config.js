4a3aec3550ea0d2bb4197343adadb7debcd4c199
Liora <liora@example.com>
2026-04-15 14:20:20 +0300

    fm

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -3,16 +3,9 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      '@babel/plugin-proposal-optional-chaining',
-      '@babel/plugin-proposal-nullish-coalescing-operator',
-      '@babel/plugin-proposal-logical-assignment-operators',
-      '@babel/plugin-transform-template-literals',
-      '@babel/plugin-transform-arrow-functions',
-      '@babel/plugin-transform-shorthand-properties',
-      '@babel/plugin-transform-block-scoping',
-      ['@babel/plugin-transform-class-properties', { "loose": true }],
-      ['@babel/plugin-transform-private-methods', { "loose": true }],
-      ['@babel/plugin-transform-private-property-in-object', { "loose": true }]
+      'react-native-worklets-core/plugin',
+      'react-native-reanimated/plugin',
     ],
   };
 };
+
